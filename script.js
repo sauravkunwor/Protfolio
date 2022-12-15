f5a8c37656e1efc0a2c5fbfebd39073574d94b16
saurav kunwor <kunworsaurav3@gmail.com>
2022-12-15 20:40:31 +0545

    Minor changes

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
 let ham = document.querySelector(".hamburger");
 
 ham.addEventListener("click", () => {
   console.log("ham clicked");
   let menu = document.getElementById("nav");
-  if (menu.style.display === "none") {
+  if (menu.style.display === "none" || !menu.style.display) {
     document.getElementById("nav").style.display = "block";
   } else {
     menu.style.display = "none";
   }
-=======
-let ham = document.querySelector(".hamburger");
-
-ham.addEventListener("click", () => {
-  console.log("ham clicked");
-  let menu = document.getElementById("nav");
-  if (menu.style.display === "none") {
-    document.getElementById("nav").style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }
->>>>>>> 9b93b46fe4f45213bc3cb79f41b4a5a2a2da3b5e
 })
